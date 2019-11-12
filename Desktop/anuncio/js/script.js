c57a074f81dda7c2732ed85b3bac474b290f2397
catarina.leitao.dengun@gmail.com <catarina.leitao.dengun@gmail.com>
2019-11-12 16:30:08 +0000

    first commit

---
+++ Desktop/anuncio/js/script.js
@@ -0,0 +1,5 @@
+function donate(){
+	alert ('Thank you for the donation!');
+	document.getElementById('donate').value = "Já doou";
+	document.getElementById('donate').style.backgroundColor = "green";
+}
